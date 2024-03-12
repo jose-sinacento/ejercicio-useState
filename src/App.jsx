7109589de64c3fd4ff9fe4ec39345c4dae14f5db
JOSE <jose@iMac-de-JOSE.local>
2024-03-12 20:00:34 +0100

    trabajo terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
- //Aquí tu código
-}
+    //Aquí tu código
+    const [name, setName] = useState("Sofía")
+    const [newName, setNewName] = useState("")
 
-export default App
+    const nameChanged = (n) => {
+        setName(n);
+    };
+
+    const changeNameToReyes = () => {
+        setName('Reyes');
+    };
+
+    const changeNameBonus = (e) => {
+        e.preventDefault();
+        if (newName) {
+        setName(newName);
+        setNewName("");
+
+        }
+    };
+
+    const handleOnChange = (e) => {
+        setNewName(e.target.value);
+    }
+
+    return (
+        <div>
+            <h1>Teacher Name: {name}</h1>
+            <ul>
+                <li onClick={() => nameChanged("Data")}>Data</li>
+                <li onClick={changeNameToReyes}>Reyes</li>
+                <li onClick={() => setName('Yolanda')}>Yolanda</li>
+            </ul>
+            <form onSubmit={changeNameBonus}>
+                <input type='text' value={newName} onChange={handleOnChange} placeholder='add a name'></input>
+                <button type='submit'>Add</button>
+            </form>
+        </div>
+    )
+};
+
+export default App;
